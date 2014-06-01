900404c79bbb45c1609fddff06e3d3b66118e1da
Shinichi Segawa <tektoh@gmail.com>
2014-06-01 14:28:29 +0900

    りふぁくたりんぐ

--- Model/Datasource/AppDescriptionSource.php
+++ Model/Datasource/AppDescriptionSource.php
@@ -6,30 +6,30 @@ App::uses('AppStoreApi', 'AppDescription.Lib');
 
 class AppDescriptionSource extends DataSource
 {
+    public $config = [
+        'googleplay' => [
+            'host' => 'play.google.com',
+            'apiClass' => 'GooglePlayApi'
+        ],
+        'appstore' => [
+            'host' => 'itunes.apple.com',
+            'apiClass' => 'AppStoreApi'
+        ]
+    ];
+    
     public function read(Model $model, $queryData = [], $recursive = null)
     {
         $url = $queryData[0];
-        $vendor = $this->_getVendor($url);
-
-        if ($vendor === 'googleplay') {
-            $api = new GooglePlayApi();
-        } else if ($vendor === 'appstore') {
-            $api = new AppStoreApi();
-        } else {
+        
+        foreach ($this->config as $config) {
+            if (strpos($url, $config['host']) != false) {
+                $api = new $config['apiClass'];
+                break;
+            }
+        }
+        if (!isset($api)) {
             return false;
         }
-
         return $api->lookup($url);
-    }
-
-    protected function _getVendor($url)
-    {
-        if (strpos($url, "itunes.apple.com") != false) {
-            return 'appstore';
-        } else if (strpos($url, "play.google.com") != false){
-            return 'googleplay';
-        } else {
-            return false;
-        }
     }
 }
